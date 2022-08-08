62a7a239771bacf24177c4f1bdf6c46ebb6ca45f
Aya Ramzy <69877513+AyaRamzy@users.noreply.github.com>
2022-08-09 00:31:12 +0200

    Update Array.c
    Adding Searching function

--- Array.c
+++ Array.c
@@ -21,3 +21,20 @@ void MinAndMax(int * arr,int size,int * Pmax,int * Pmin)
     *Pmin = min;
 
 }
+/****************************************************************************************/
+/*C function to return the index of LAST occurrence of a number in a 
+given array. Array index start from 0. If the item is not in the list return -1*/
+/***************************************************************************************/
+int Array_Search(int * arr,int size,int num)
+{
+    int i,index=-1;
+    for(i=0;i<size;i++)
+    {
+        if(arr[i]== num)
+        {
+            index = i;
+        }
+    }
+    return index;
+
+}
